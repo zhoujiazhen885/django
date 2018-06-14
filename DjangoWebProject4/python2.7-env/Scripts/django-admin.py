3629a1b292dc508c8aba4ae272da9617097c9cb3
佳振 周 <443902617@qq.com>
2018-06-14 18:42:27 +0800

    第一次提交，增加了test.html

---
+++ DjangoWebProject4/python2.7-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\share\DjangoWebProject4\DjangoWebProject4\python2.7-env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
