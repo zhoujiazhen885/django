774020aa566ea40a118bd39340c3a3643af7d5b5
佳振 周 <443902617@qq.com>
2018-06-19 11:33:18 +0800

    更新js

--- DjangoWebProject4/app/static/app/scripts/test.js
+++ DjangoWebProject4/app/static/app/scripts/test.js
@@ -1,3 +1,11 @@
 function type(o) {
     return (o == null) ? "null" : (typeof o);
 }
+
+loop: for (var j = 1; j < 6; j++) {
+    document.write("<br>" + j + ":");
+    for (var i = 1; i < 6; i++) {
+        if (i == 3) continue loop;    //当i=3的时候，重新循环loop
+        document.write(i);
+    }
+}
